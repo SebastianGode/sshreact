3593aea98e69ef7621059d4e454e0439464fb117
Sebastian Gode <sebastian.gode@telekom.de>
2021-08-03 11:30:31 +0000

    Docker for MariaDB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import NavBar from './Navbar';
 import Footer from './Footer.jsx';
 import reportWebVitals from './reportWebVitals';
