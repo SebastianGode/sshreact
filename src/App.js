afab7eaf3f957920ee1df31a96ff25b3729ae0cc
Sebastian Gode <sebastian.gode@telekom.de>
2021-08-03 10:06:45 +0000

    Sign-Up-Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { ScaleButton } from '@telekom/scale-components-react';
+import { ScaleTextField, ScaleButton } from '@telekom/scale-components-react';
 
 function App() {
   return (
     <div>
-      <ScaleButton>test</ScaleButton>
+      <form action="action_page.php">
+        <div class="container">
+          <h1 class="Sign-Up">Sign Up</h1>
+          <p>Please fill in this form to create an account.</p>
+          
+          <ScaleTextField id="email" label="E-Mail" required></ScaleTextField>
+          <p></p>
+          <ScaleTextField id="password" label="Password" type="password" required></ScaleTextField>
+          <p></p>
+          <ScaleTextField id="passwordrepeat" label="Repeat Password" type="password" required></ScaleTextField>
+
+          <p>By creating an account you agree to our <a href="#" >Terms & Privacy</a>.</p>
+          <ScaleButton type="submit"> Register</ScaleButton>
+        </div>
+      </form>
     </div> 
   );
 }
