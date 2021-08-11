9e8450cd0fb30c13b57fc8c8537a821cda948296
Sebastian Gode <sebastian.gode@telekom.de>
2021-08-11 08:01:44 +0000

    added auth.json

--- api/api.py
+++ api/api.py
@@ -1,16 +1,16 @@
-import time
+import mysql.connector as database
 import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/time')
-def get_current_time():
-    return {'time': time.time()}
-
 @app.route('/api/register', methods=['POST'])
 def register():
     record = json.loads(request.data)
-    with open('data.txt', 'a') as f:
-        f.write(record["auth"]["password"] + " " + record["auth"]["email"] + "\n")
+    # with open('data.txt', 'a') as f:
+    #     f.write(record["auth"]["password"] + " " + record["auth"]["email"] + "\n")
+    dbpass = json.load(open('auth.json', 'r'))
+    username = dbpass["username"]
+    password = dbpass["password"]
+
     return jsonify(record)
