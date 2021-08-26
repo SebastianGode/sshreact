dadda9bbd81376c6b5893e378ad5f91b162d3816
Sebastian Gode <sebastian.gode@telekom.de>
2021-08-26 11:51:55 +0000

    Fixed errors

--- src/Verify.jsx
+++ src/Verify.jsx
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function Verify() {
-    
-    const [content, setContent] = useState("");
-    useEffect(() => {
-        const urlParams = new URLSearchParams(window.location.search)
-        const jsontoken = {
-          "verify": {
-            "token": urlParams.get('token')
+  const Content = () => {
+      const [content, setContent] = useState("");
+      useEffect(() => {
+          const urlParams = new URLSearchParams(window.location.search)
+          const jsontoken = {
+            "verify": {
+              "token": urlParams.get('token')
+            }
           }
-        }
-        setContent(
-          <h1>Checking Token...</h1>
-        )
-        axios.post('/api/verify', jsontoken)
-            .then((response) => {
-              setContent(
-                <h1>Verification successful! You will be redirected in 5 seconds.</h1>
-              )
-              setTimeout(() => {
-                window.location.replace("/login");   
-              }, 5000)
-            })
-            .catch((reason) => {
-              console.log(reason.response.data)
-              setContent(
-                <h1>Error occured</h1>
-              )
-              alert(reason.response.data.verification.error)
-              window.location.replace("/register");
-            })
-    }, [])
+          setContent(
+            <h1>Checking Token...</h1>
+          )
+          axios.post('/api/verify', jsontoken)
+              .then((response) => {
+                setContent(
+                  <h1>Verification successful! You will be redirected in 5 seconds.</h1>
+                )
+                setTimeout(() => {
+                  window.location.replace("/login");   
+                }, 5000)
+              })
+              .catch((reason) => {
+                console.log(reason.response.data)
+                setContent(
+                  <h1>Error occured</h1>
+                )
+                alert(reason.response.data.verification.error)
+                window.location.replace("/register");
+              })
+      }, [])
+      return content
+    }
   return (
-    <div>
+    <div class="centered">
       <Content/>
     </div> 
   );
