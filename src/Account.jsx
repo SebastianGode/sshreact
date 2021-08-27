60810713d049bae81503c3e88058bb01fcac359f
Sebastian Gode <sebastian.gode@telekom.de>
2021-08-27 09:48:29 +0000

    Added Account Delete Page functionality

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,14 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import { ScaleButton } from '@telekom/scale-components-react';
 
 function Account() {
   const Content = () => {
       const [content, setContent] = useState("");
       useEffect(() => {
-          const urlParams = new URLSearchParams(window.location.search)
           const jsontoken = {
             "verify": {
-              "token": urlParams.get('token')
+              "token": sessionStorage.getItem('token')
             }
           }
           setContent(
@@ -17,17 +17,35 @@ function Account() {
           axios.post('/api/verifylogin', jsontoken)
               .then((response) => {
                 setContent(
-                  <h1>Verification successful!</h1>
+                    <div class="centered">
+                        <h3>Do you want to delete your account?</h3>
+                        <ScaleButton type="button" id="del-btn">Delete Account</ScaleButton>
+                    </div>
                 )
+                document.getElementById("del-btn").addEventListener("click", function(){
+                    setContent(
+                        <div class="centered">
+                            <h3>Deleting account...</h3>
+                        </div>
+                    )
+                    axios.post('/api/deleteaccount', jsontoken)
+                        .then((response) => {
+                            alert("Account successfully deleted")
+                            window.location.replace("/");
+                        })
+                        .catch((reason) => {
+                            alert("Something went wrong")
+                        })
+                }, false);
               })
               .catch((reason) => {
                 console.log(reason.response.data)
                 setContent(
                   <h1>You need to sign-in first! Redirecting...</h1>
                 )
-                setTimeout(() => {
-                    window.location.replace("/login");
-                  }, 1000)
+                // setTimeout(() => {
+                //     window.location.replace("/login");
+                //   }, 1000)
               })
       }, [])
       return content
